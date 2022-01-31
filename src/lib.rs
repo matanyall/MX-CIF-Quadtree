77f56824709e917a44480f7228abc70d8433dd78
Matanya Loewenthal <matanya@loewenthal.net>
2022-01-31 09:47:15 -0700

    feat(docs): Wrote comments

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,20 @@ mod tests {
 }
 
 
-mod MX_CIF_Quadtree {
+pub mod MX_CIF_Quadtree {
 
+    /// Number of directions in 1d space
     const NDIR_1D: usize = 2;
+    /// Number of directions in 2d space
     const NDIR_2D: usize = 4;
 
+    /// Axis enum for 2d space
     enum axis {
         X,
         Y
     }
 
+    /// Quadrants in our Quadtree, uses this ordering in traversal
     enum quadrant {
         NW,
         NE,
@@ -25,35 +29,61 @@ mod MX_CIF_Quadtree {
         SE
     }
 
+    /// Direction enum for traversing Binary tree of rectangles
     enum direction {
         LEFT,
         RIGHT,
         BOTH
     }
 
+    /// Rectangle struct (represented with center, length & width)
     struct Rectangle {
+        /// name of the rectangle
         name: String,
+        /// center of the rectangle (Centroid)
         center: [f64; 2],
+        /// Distance to borders or rectangle
         length: [f64; 2],
+        /// Left and right sons
         bin_son: [Option<Box<Rectangle>>; NDIR_1D],
+        /// Used in component labeling
         label: usize
     }
 
+    /// Binary Node struct
     struct BNode {
+        /// Left and right sons
         bin_son: [Option<Box<BNode>>; NDIR_2D],
+        /// Pointer to the rectangle whose area contains the axis subdivision point
         rect: Option<Box<Rectangle>>,
         is_black: bool
     }
 
+    /// Cif Node struct
     struct CNode {
+        /// Four principal quad directions
         spc_son: [Option<Box<CNode>>; NDIR_2D],
+        /// Pointers to rectangle sets for each of the axis
         bin_son: [Option<Box<BNode>>; NDIR_1D],
     }
 
+    /// Quadtree struct
     struct MxCifQuadtree {
         root: Option<Box<CNode>>,
+        /// World extent
         world: Rectangle,
         id: usize
+    }
+
+    impl MxCifQuadtree {
+        pub fn new(world: Rectangle) -> MxCifQuadtree {
+            MxCifQuadtree {
+                root: None,
+                world: world,
+                id: 0
+            }
+        }
+
     }
 
 
